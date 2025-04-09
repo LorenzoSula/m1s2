d46d500f0116af6189038b780d9b82fe32c52c66
LorenzoSula <lorenzosula05@gmail.com>
2025-04-09 15:21:51 +0200

    Add files via upload

---
+++ pratica.py
@@ -0,0 +1,4 @@
+città = input("Inserisci la tua città di origine: ")
+animale = input("Inserisci il nome del tuo animale domestico: ")
+nomeBand = città + " " + animale
+print("Il nome per la tua band potrebbe essere: " + nomeBand)
